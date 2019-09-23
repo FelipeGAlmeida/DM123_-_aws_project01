33a0a8488640fdffe874314a003fb279775bd4f5
Felipe G. Almeida <felipegalmeida@outlook.com>
2019-09-23 08:50:09 -0300

    Logs Added

--- src/main/java/br/com/dm123/aws_project01/AwsProject01Application.java
+++ src/main/java/br/com/dm123/aws_project01/AwsProject01Application.java
@@ -3,11 +3,18 @@ package br.com.dm123.aws_project01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class AwsProject01Application {
 
+	private static final Logger log = LoggerFactory.getLogger(AwsProject01Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AwsProject01Application.class, args);
+
+		log.info("Starting AWS_Project01 example");
 	}
 
 }
